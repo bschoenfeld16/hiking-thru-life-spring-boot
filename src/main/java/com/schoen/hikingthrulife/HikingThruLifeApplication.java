09477c5199f3b2b391170f22776521fe85f4e86f
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2019-12-27 11:05:23 -0600

    init commit

---
+++ src/main/java/com/schoen/hikingthrulife/HikingThruLifeApplication.java
@@ -0,0 +1,12 @@
+package com.schoen.hikingthrulife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingThruLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingThruLifeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/schoen/hikingthrulife/HikingThruLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoen.hikingthrulife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingThruLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
