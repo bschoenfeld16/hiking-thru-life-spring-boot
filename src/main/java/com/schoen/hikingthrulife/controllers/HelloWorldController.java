cc6bfcd5453e04da597d0042cb9ae9a40947942b
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2019-12-27 17:18:45 -0600

    Cleaning up. Realized I do not need the aws dependencies

--- src/main/java/com/schoen/hikingthrulife/controllers/HelloWorldController.java
+++ src/main/java/com/schoen/hikingthrulife/controllers/HelloWorldController.java
@@ -1,13 +1,28 @@
 package com.schoen.hikingthrulife.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 @RestController
 public class HelloWorldController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/react")
+    public String getReactApp() {
+        return new RestTemplate().getForEntity("https://hiking-thru-life-react.s3-us-west-2.amazonaws.com/index.html", String.class).getBody();
     }
 }
